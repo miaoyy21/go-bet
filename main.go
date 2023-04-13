bdeba65c46dec6fd3a60cf1ee8c117bc48902fe4
miaojingyi <miaojingyi86@gmail.com>
2023-04-14 03:15:08 +0800

    .

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ type Config struct {
 }
 
 func main() {
-	log.Printf("当前版本 2023.04.13 15:10\n")
+	log.Printf("当前版本 2023.04.14 03:10\n")
 
 	dir, err := os.Getwd()
 	if err != nil {
--- xmd/analysis4.go
+++ xmd/analysis4.go
@@ -11,7 +11,7 @@ import (
 var latest = make(map[int]struct{})
 var wins int
 var fails int
-var rate = 5.25
+var rate = 2.25
 var times = 1
 
 func analysis(cache *Cache) error {
@@ -27,36 +27,36 @@ func analysis(cache *Cache) error {
 		return err
 	}
 
-	for i := len(cache.histories) - 1; i >= len(cache.histories)-8; i-- {
-		result := cache.histories[i].result
-		if result <= 5 || result >= 22 {
-			log.Printf("【%-4d】第【🐔 %d %03d/%03d】期：开奖结果【%d】，余额【%d】，开始执行分析 ...\n", times, cache.issue, wins, fails, cache.result, surplus)
-			if len(latest) > 0 {
-				if _, exists := latest[cache.result]; !exists {
-					fails++
-					rate = rate * 1.625
-				}
-			}
-
-			latest = make(map[int]struct{})
-			total, coverage := 0, 0
-			for _, n := range []int{0, 1, 2, 3, 4, 5, 22, 23, 24, 25, 26, 27} {
-				betGold := int(float64(cache.user.gold) * float64(stds[n]) / 1000)
-				if err := hPostBet(nextIssue, betGold, n, cache.user); err != nil {
-					return err
-				}
-				log.Printf("第【%s】期：竞猜数字【👍 %02d】，标准赔率【%-7.2f】，投注金额【% 5d】\n", nextIssue, n, 1000.0/float64(stds[n]), betGold)
-
-				total = total + betGold
-				coverage = coverage + stds[n]
-			}
-
-			times++
-			surplus = surplus - total
-			log.Printf("第【%s】期：投注数字【0 ~ 5, 22 ~ 27】，投注金额【%d】，余额【%d】，覆盖率【%.2f%%】 >>>>>>>>>> \n", nextIssue, total, surplus, float64(coverage)/10)
-			return nil
-		}
-	}
+	//for i := len(cache.histories) - 1; i >= len(cache.histories)-8; i-- {
+	//	result := cache.histories[i].result
+	//	if result <= 5 || result >= 22 {
+	//		log.Printf("【%-4d】第【🐔 %d %03d/%03d】期：开奖结果【%d】，余额【%d】，开始执行分析 ...\n", times, cache.issue, wins, fails, cache.result, surplus)
+	//		if len(latest) > 0 {
+	//			if _, exists := latest[cache.result]; !exists {
+	//				fails++
+	//				rate = rate * 1.625
+	//			}
+	//		}
+	//
+	//		latest = make(map[int]struct{})
+	//		total, coverage := 0, 0
+	//		for _, n := range []int{0, 1, 2, 3, 4, 5, 22, 23, 24, 25, 26, 27} {
+	//			betGold := int(float64(cache.user.gold) * float64(stds[n]) / 1000)
+	//			if err := hPostBet(nextIssue, betGold, n, cache.user); err != nil {
+	//				return err
+	//			}
+	//			log.Printf("第【%s】期：竞猜数字【👍 %02d】，标准赔率【%-7.2f】，投注金额【% 5d】\n", nextIssue, n, 1000.0/float64(stds[n]), betGold)
+	//
+	//			total = total + betGold
+	//			coverage = coverage + stds[n]
+	//		}
+	//
+	//		times++
+	//		surplus = surplus - total
+	//		log.Printf("第【%s】期：投注数字【0 ~ 5, 22 ~ 27】，投注金额【%d】，余额【%d】，覆盖率【%.2f%%】 >>>>>>>>>> \n", nextIssue, total, surplus, float64(coverage)/10)
+	//		return nil
+	//	}
+	//}
 
 	// 按照尾数取最热的8期
 	w8s, w2s := make(map[int]struct{}), make(map[int]struct{})
@@ -91,6 +91,16 @@ func analysis(cache *Cache) error {
 
 	// 输出
 	if len(latest) == 0 {
+		for i := len(cache.histories) - 1; i >= len(cache.histories)-8; i-- {
+			result := cache.histories[i].result
+			if result <= 5 || result >= 22 {
+				latest = make(map[int]struct{})
+
+				log.Printf("【%-4d】第【🐔 %d %03d/%03d】期：开奖结果【%d】，余额【%d】，暂时停止投注【%d】 ...\n", times, cache.issue, wins, fails, cache.result, surplus, len(cache.histories)-i)
+				return nil
+			}
+		}
+
 		log.Printf("【%-4d】第【✊ %d %03d/%03d】期：开奖结果【%d】，余额【%d】，投注尾数【%s】，开始执行分析 ...\n", times, cache.issue, wins, fails, cache.result, surplus, strings.Join(sw8s, ","))
 	} else {
 		if _, exists := latest[cache.result]; exists {
@@ -103,7 +113,18 @@ func analysis(cache *Cache) error {
 			log.Printf("【%-4d】第【👍 %d %03d/%03d】期：开奖结果【%d】，余额【%d】，投注尾数【%s】，开始执行分析 ...\n", times, cache.issue, wins, fails, cache.result, surplus, strings.Join(sw8s, ","))
 		} else {
 			fails++
-			rate = rate * 1.625
+			rate = rate * 1.675
+
+			for i := len(cache.histories) - 1; i >= len(cache.histories)-8; i-- {
+				result := cache.histories[i].result
+				if result <= 5 || result >= 22 {
+					latest = make(map[int]struct{})
+
+					log.Printf("【%-4d】第【🐔 %d %03d/%03d】期：开奖结果【%d】，余额【%d】，暂时停止投注【%d】 ...\n", times, cache.issue, wins, fails, cache.result, surplus, len(cache.histories)-i)
+					return nil
+				}
+			}
+
 			log.Printf("【%-4d】第【👀 %d %03d/%03d】期：开奖结果【%d】，余额【%d】，投注尾数【%s】，开始执行分析 ...\n", times, cache.issue, wins, fails, cache.result, surplus, strings.Join(sw8s, ","))
 		}
 	}
@@ -119,7 +140,7 @@ func analysis(cache *Cache) error {
 		// 倍率变化率
 		delta := 1.0
 		if _, exists := w2s[result%10]; exists {
-			delta = 1.075
+			delta = 1.1
 		}
 
 		betGold := int(rate * delta * float64(cache.user.gold) * float64(stds[result]) / 1000)
