39db91984cd988ad02f56db9d8b2d40a01b53f9c
miaojingyi <miaojingyi86@gmail.com>
2023-05-03 22:44:42 +0800

    .

--- analysis.sql
+++ analysis.sql
@@ -27,29 +27,33 @@ FROM (
 ORDER BY TT.result ASC
 
 -- 按照返奖率统计指定日期输赢情况
-SELECT '[0, 0.85]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-01 %' AND rx <= 0.85
+SELECT '[0, 0.95]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold*dx0),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-03 %' AND rx <= 0.95
 UNION ALL
-SELECT '(0.85, 0.90]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-01 %' AND rx > 0.85 AND rx <= 0.90
+SELECT '(0.95, 0.96]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold*dx0),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-03 %' AND rx > 0.95 AND rx <= 0.96
 UNION ALL
-SELECT '(0.90, 0.95]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-01 %' AND rx > 0.90 AND rx <= 0.95
+SELECT '(0.96, 0.97]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold*dx0),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-03 %' AND rx > 0.96 AND rx <= 0.97
 UNION ALL
-SELECT '(0.95, 1.00]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-01 %' AND rx > 0.95 AND rx <= 1.00
+SELECT '(0.97, 0.98]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold*dx0),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-03 %' AND rx > 0.97 AND rx <= 0.98
 UNION ALL
-SELECT '(1.00, 1.05]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-01 %' AND rx > 1.00 AND rx <= 1.05
+SELECT '(0.98, 0.99]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold*dx0),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-03 %' AND rx > 0.98 AND rx <= 0.99
 UNION ALL
-SELECT '(1.05, 1.10]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-01 %' AND rx > 1.05 AND rx <= 1.10
+SELECT '(0.99, 1.00]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold*dx0),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-03 %' AND rx > 0.99 AND rx <= 1.00
 UNION ALL
-SELECT '(1.10, 1.15]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-01 %' AND rx > 1.10 AND rx <= 1.15
+SELECT '(1.00, 1.01]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold*dx0),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-03 %' AND rx > 1.00 AND rx <= 1.01
 UNION ALL
-SELECT '(1.15, 1.20]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-01 %' AND rx > 1.15 AND rx <= 1.20
+SELECT '(1.01, 1.02]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold*dx0),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-03 %' AND rx > 1.01 AND rx <= 1.02
 UNION ALL
-SELECT '(1.20, 1.25]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-01 %' AND rx > 1.20 AND rx <= 1.25
+SELECT '(1.02, 1.03]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold*dx0),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-03 %' AND rx > 1.02 AND rx <= 1.03
 UNION ALL
-SELECT '(1.25, 5]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-01 %' AND rx > 1.25
+SELECT '(1.03, 1.04]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold*dx0),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-03 %' AND rx > 1.03 AND rx <= 1.04
+UNION ALL
+SELECT '(1.04, 1.05]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold*dx0),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-03 %' AND rx > 1.04 AND rx <= 1.05
+UNION ALL
+SELECT '(1.05, 5.00]' AS scope, CONVERT(SUM(win_gold)/AVG(user_gold*dx0),DECIMAL(13,2)) AS rate FROM logs WHERE time LIKE '2023-05-03 %' AND rx > 1.05
 
 -- 按照统计指定日期的每个小时输赢情况
-SELECT LEFT(time,13),CONVERT(SUM(win_gold)/AVG(user_gold),DECIMAL(13,2)) AS rate
+SELECT LEFT(time,13),CONVERT(SUM(win_gold)/AVG(user_gold*dx0),DECIMAL(13,2)) AS rate
 FROM logs
-WHERE time LIKE '2023-05-01 %'
+WHERE time LIKE '2023-05-03 %'
 GROUP BY LEFT(time,13);
 
