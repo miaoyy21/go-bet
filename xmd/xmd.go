79ded2ec46ff47cda0672e72d4b4b3906e747765
miaojingyi <miaojingyi86@gmail.com>
2023-05-04 00:05:36 +0800

    .

--- xmd/xmd.go
+++ xmd/xmd.go
@@ -12,11 +12,11 @@ var SN28 = []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 1
 
 func Run(cache *Cache) {
 	log.Printf("当前设置活动状态（%t） ...\n", cache.isExtra)
-	log.Printf("当前设置投注基数为 %d ...\n", cache.user.gold)
 	log.Printf("当前设置当不存在超过实际赔率%.2f%%的数字时，仅进行全部投注 ...\n", cache.wx*100-100)
 	log.Printf("当前设置当返奖率不超过%.2f%%时，仅进行全部投注 ...\n", cache.rx*100)
 	log.Printf("当前设置本时内中奖比率变化量达到设定标准时，基础投注变为%.2f...\n", cache.ex)
 	log.Printf("当前设置连续投注失败3次时，本期投注倍率变为%.2f倍...\n", cache.dx)
+	log.Printf("当前设置投注基数为 %d ...\n", cache.user.gold)
 	if cache.user.isDebug {
 		log.Println("当前设置为调试模式，不发送投注请求 ...")
 	}
