e5daf8e51c52d5d5787dbc4827d024ab2f74852c
miaojingyi <miaojingyi86@gmail.com>
2023-05-17 17:16:06 +0800

    .

--- xmd/xmd_test.go
+++ xmd/xmd_test.go
@@ -9,11 +9,6 @@ import (
 )
 
 func TestCache_Sync2(t *testing.T) {
-
-	dbHost, dbPass := "www.nsmei.com", "envl11a3ANsxZNCFQA0GO9HLObf8gi5"
-	userId := "31783106"
-	dt := "2023-05-16"
-
 	// MySQL
 	ds := fmt.Sprintf("root:%s@tcp(%s:3306)/bet?charset=utf8mb4&collation=utf8mb4_general_ci&loc=Local&parseTime=true", dbPass, dbHost)
 	db, err := sql.Open("mysql", ds)
@@ -37,13 +32,13 @@ func TestCache_Sync2(t *testing.T) {
 
 		var sn int
 		var sr float64
-		qs := fmt.Sprintf("SELECT COUNT(1) AS nn,IFNULL(CONVERT(SUM(win_gold * IFNULL(rz,1.0))/AVG(user_gold * IFNULL(rz,1.0)),DECIMAL(13,2)),1.0) AS rate FROM logs_%s WHERE issue >= ? - 10 and issue < ?", userId)
+		qs := fmt.Sprintf("SELECT COUNT(1) AS nn,IFNULL(CONVERT(SUM(win_gold * IFNULL(rz,1.0))/AVG(user_gold * IFNULL(rz,1.0))/AVG(IFNULL(rz,1.0)),DECIMAL(13,2)),1.0) AS rate FROM logs_%s WHERE issue >= ? - 5 and issue < ?", userId)
 		if err := db.QueryRow(qs, ris, ris).Scan(&sn, &sr); err != nil {
 			t.Fatalf("db.QueryRow(%d) fail :: %s \n", ris, err.Error())
 		}
 
 		rate := 1.0
-		if sn >= 8 {
+		if sn >= 4 {
 			rate = math.Pow(1.25, sr)
 		}
 
@@ -52,6 +47,6 @@ func TestCache_Sync2(t *testing.T) {
 			t.Fatalf("db.Exec() fail :: %s \n", err.Error())
 		}
 
-		t.Logf("Update Issue of [%.2f%%] %d %.2f Successful ! \n", float64((i+1)*100)/float64(len(res)), ris, rate)
+		t.Logf("Update Issue of %d/%d [%.2f%%] %d %.2f Successful ! \n", i+1, len(res), float64((i+1)*100)/float64(len(res)), ris, rate)
 	}
 }
