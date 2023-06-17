767baa9b9b0567237306f8d7b1ba66ca42d5ee95
miaojingyi <miaojingyi86@gmail.com>
2023-06-17 19:46:58 +0800

    .

--- xmd/fn_analysis.go
+++ xmd/fn_analysis.go
@@ -60,7 +60,7 @@ func analysis(cache *Cache) error {
 	log.Printf("第【%s】期：预投注数字【%s】 >>>>>>>>>> \n", nextIssue, fmtIntSlice(rs))
 
 	// 确定投注模式ID
-	modeId := parseModeId(bets)
+	modeId, modeName := parseModeId(bets)
 	if modeId <= 0 {
 		log.Printf("第【%s】期：无法确定投注模式ID【%d】 >>>>>>>>>> \n", nextIssue, modeId)
 		return nil
@@ -70,76 +70,74 @@ func analysis(cache *Cache) error {
 	if err := hModesBetting(nextIssue, modeId, cache.user); err != nil {
 		return err
 	}
-	log.Printf("第【%s】期：投注模式ID【%d】，投注成功 >>>>>>>>>> \n", nextIssue, modeId)
+	log.Printf("第【%s】期：投注模式【%s】，投注成功 >>>>>>>>>> \n", nextIssue, modeName)
 
 	return nil
 }
 
-var modeNums = map[int]float64{}
-
-func parseModeId(bets map[int]float64) int {
-	var m1, m2, m3, m4, m5, m6, m7, m8 float64
+func parseModeId(bets map[int]float64) (int, string) {
+	var m1, m2, m3, m4, m5, m6, m7, m8 int
 	for result := range bets {
 		if result >= 14 {
-			m1 += float64(stds[result])
+			m1 += stds[result]
 		} else {
-			m2 += float64(stds[result])
+			m2 += stds[result]
 		}
 
 		if result%2 == 1 {
-			m3 += float64(stds[result])
+			m3 += stds[result]
 		} else {
-			m4 += float64(stds[result])
+			m4 += stds[result]
 		}
 
 		if result >= 10 && result <= 17 {
-			m5 += float64(stds[result])
+			m5 += stds[result]
 		} else {
-			m6 += float64(stds[result])
+			m6 += stds[result]
 		}
 
 		if result%10 >= 5 && result%10 <= 9 {
-			m7 += float64(stds[result])
+			m7 += stds[result]
 		} else {
-			m8 += float64(stds[result])
+			m8 += stds[result]
 		}
 	}
 
-	m5 *= float64(44) / 56
-	m6 *= float64(56) / 44
-	log.Printf("权重：1[%9.2f], 2[%9.2f], 3[%9.2f], 4[%9.2f], 5[%9.2f], 6[%9.2f], 7[%9.2f], 8[%9.2f] \n", m1, m2, m3, m4, m5, m6, m7, m8)
+	m5 = int(float64(m5*44) / 56)
+	m6 = int(float64(m5*56) / 44)
+	log.Printf("模式权重：大数【%d】, 小数【%d】, 奇数【%d】, 偶数【%d】, 中数【%d】, 边数【%d】, 大尾数【%d】, 小尾数【%d】 \n", m1, m2, m3, m4, m5, m6, m7, m8)
 
 	if m1 >= 350 && m1 >= m2 && m1 >= m3 && m1 >= m4 && m1 >= m5 && m1 >= m6 && m1 >= m7 && m1 >= m8 {
-		return 1
+		return 1, "大数"
 	}
 
 	if m2 >= 350 && m2 >= m1 && m2 >= m3 && m2 >= m4 && m2 >= m5 && m2 >= m6 && m2 >= m7 && m2 >= m8 {
-		return 2
+		return 2, "小数"
 	}
 
 	if m3 >= 350 && m3 >= m1 && m3 >= m2 && m3 >= m4 && m3 >= m5 && m3 >= m6 && m3 >= m7 && m3 >= m8 {
-		return 3
+		return 3, "奇数"
 	}
 
 	if m4 >= 350 && m4 >= m1 && m4 >= m2 && m4 >= m3 && m4 >= m5 && m4 >= m6 && m4 >= m7 && m4 >= m8 {
-		return 4
+		return 4, "偶数"
 	}
 
 	if m5 >= 350 && m5 >= m1 && m5 >= m2 && m5 >= m3 && m5 >= m4 && m5 >= m6 && m5 >= m7 && m5 >= m8 {
-		return 5
+		return 5, "中数"
 	}
 
 	if m6 >= 350 && m6 >= m1 && m6 >= m2 && m6 >= m3 && m6 >= m4 && m6 >= m5 && m6 >= m7 && m6 >= m8 {
-		return 6
+		return 6, "边数"
 	}
 
 	if m7 >= 350 && m7 >= m1 && m7 >= m2 && m7 >= m3 && m7 >= m4 && m7 >= m5 && m7 >= m6 && m7 >= m8 {
-		return 7
+		return 7, "大尾数"
 	}
 
 	if m8 >= 350 && m8 >= m1 && m8 >= m2 && m8 >= m3 && m8 >= m4 && m8 >= m5 && m8 >= m6 && m8 >= m7 {
-		return 8
+		return 8, "小尾数"
 	}
 
-	return 0
+	return 0, "未知"
 }
