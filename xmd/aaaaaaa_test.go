4d09012d780a9a20f86da90fb297f5d32cec0364
miaojingyi <miaojingyi86@gmail.com>
2023-06-17 12:28:22 +0800

    .

---
+++ xmd/aaaaaaa_test.go
@@ -0,0 +1,10 @@
+package xmd
+
+import (
+	"log"
+	"testing"
+)
+
+func TestCache_Sync(t *testing.T) {
+	log.Println(float64(44) / 56)
+}
--- xmd/fn_analysis.go
+++ xmd/fn_analysis.go
@@ -41,7 +41,7 @@ func analysis(cache *Cache) error {
 			rx = (r1/r0 - 0.99) * 100
 		}
 
-		if rx <= 0.0001 {
+		if rx <= 0.01 {
 			log.Printf("第【%s】期：竞猜数字【   %02d】，标准赔率【%-7.2f】，实际赔率【%-7.2f】，赔率系数【%-6.4f】\n", nextIssue, result, r0, r1, r1/r0)
 			continue
 		}
@@ -76,62 +76,66 @@ func analysis(cache *Cache) error {
 }
 
 func parseModeId(bets map[int]float64) int {
-	m1, m2, m3, m4, m5, m6, m7, m8 := 0, 0, 0, 0, 0, 0, 0, 0
+	var m1, m2, m3, m4, m5, m6, m7, m8 float64
 	for result := range bets {
 		if result >= 14 {
-			m1++
+			m1 += float64(stds[result])
 		} else {
-			m2++
+			m2 += float64(stds[result])
 		}
 
 		if result%2 == 1 {
-			m3++
+			m3 += float64(stds[result])
 		} else {
-			m4++
+			m4 += float64(stds[result])
 		}
 
 		if result >= 10 && result <= 17 {
-			m5++
+			m5 += float64(stds[result])
 		} else {
-			m6++
+			m6 += float64(stds[result])
 		}
 
 		if result%10 >= 5 && result%10 <= 9 {
-			m7++
+			m7 += float64(stds[result])
 		} else {
-			m8++
+			m8 += float64(stds[result])
 		}
 	}
 
-	if m1 >= m2 && m1 >= m3 && m1 >= m4 && m1 >= m5 && m1 >= m6 && m1 >= m7 && m1 >= m8 {
+	m5 *= float64(44) / 56
+	m6 *= float64(56) / 44
+	log.Printf("权重：1[%9.2f], 2[%9.2f], 3[%9.2f], 4[%9.2f], 5[%9.2f], 6[%9.2f], 7[%9.2f], 8[%9.2f] \n", m1, m2, m3, m4, m5, m6, m7, m8)
+
+	if m1 >= 500*0.9 && m1 >= m2 && m1 >= m3 && m1 >= m4 && m1 >= m5 && m1 >= m6 && m1 >= m7 && m1 >= m8 {
 		return 1
 	}
 
-	if m2 >= m1 && m2 >= m3 && m2 >= m4 && m2 >= m5 && m2 >= m6 && m2 >= m7 && m2 >= m8 {
+	if m2 >= 500*0.9 && m2 >= m1 && m2 >= m3 && m2 >= m4 && m2 >= m5 && m2 >= m6 && m2 >= m7 && m2 >= m8 {
 		return 2
 	}
 
-	if m3 >= m1 && m3 >= m2 && m3 >= m4 && m3 >= m5 && m3 >= m6 && m3 >= m7 && m3 >= m8 {
+	if m3 >= 500*0.9 && m3 >= m1 && m3 >= m2 && m3 >= m4 && m3 >= m5 && m3 >= m6 && m3 >= m7 && m3 >= m8 {
 		return 3
 	}
 
-	if m4 >= m1 && m4 >= m2 && m4 >= m3 && m4 >= m5 && m4 >= m6 && m4 >= m7 && m4 >= m8 {
+	if m4 >= 500*0.9 && m4 >= m1 && m4 >= m2 && m4 >= m3 && m4 >= m5 && m4 >= m6 && m4 >= m7 && m4 >= m8 {
 		return 4
 	}
 
-	if m5 >= m1 && m5 >= m2 && m5 >= m3 && m5 >= m4 && m5 >= m6 && m5 >= m7 && m5 >= m8 {
+	if m5 >= 500*0.9 && m5 >= m1 && m5 >= m2 && m5 >= m3 && m5 >= m4 && m5 >= m6 && m5 >= m7 && m5 >= m8 {
 		return 5
 	}
 
-	if m6 >= m1 && m6 >= m2 && m6 >= m3 && m6 >= m4 && m6 >= m5 && m6 >= m7 && m6 >= m8 {
+	if m6 >= 500*0.9 && m6 >= m1 && m6 >= m2 && m6 >= m3 && m6 >= m4 && m6 >= m5 && m6 >= m7 && m6 >= m8 {
 		return 6
 	}
 
-	if m7 >= m1 && m7 >= m2 && m7 >= m3 && m7 >= m4 && m7 >= m5 && m7 >= m6 && m7 >= m8 {
+	if m7 >= 500*0.9 && m7 >= m1 && m7 >= m2 && m7 >= m3 && m7 >= m4 && m7 >= m5 && m7 >= m6 && m7 >= m8 {
 		return 7
 	}
 
-	if m8 >= m1 && m8 >= m2 && m8 >= m3 && m8 >= m4 && m8 >= m5 && m8 >= m6 && m8 >= m7 {
+	if m8 >= 500*0.9 && m8 >= m1 && m8 >= m2 && m8 >= m3 && m8 >= m4 && m8 >= m5 && m8 >= m6 && m8 >= m7 {
 		return 8
 	}
 
